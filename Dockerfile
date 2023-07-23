db0ffc2c0b7f01e8c414781f4bd58ae47251cc5d
Jason A Everling <jason.everling@gmail.com>
2023-07-22 21:09:58 -0500

    add initial build

---
+++ Dockerfile
@@ -0,0 +1,81 @@
+FROM ubuntu:latest
+
+MAINTAINER jason.everling@gmail.com
+
+ARG TZ=America/North_Dakota/Center
+ARG PHP_VERSION=8.1
+ARG SQL_VERSION=5.11.0
+
+RUN set eux; \
+    ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone; \
+    apt-get update && apt-get install --no-install-recommends -y \
+    ca-certificates \
+    curl \
+    jq \
+    wget \
+    libaio1 \
+    libcurl4 \
+    libgss3 \
+    libmcrypt-dev \
+    libxml2 \
+    libxslt1.1 \
+    libzip-dev \
+    locales \
+    sassc \
+    unixodbc \
+    unixodbc-dev \
+    unzip \
+    zip \
+    apache2 \
+    php \
+    libapache2-mod-php \
+    php-bcmath \
+    php-curl \
+    php-cli \
+    php-dev \
+    php-gd \
+    php-intl \
+    php-json \
+    php-ldap \
+    php-mbstring \
+    php-mysql \
+    php-odbc \
+    php-opcache \
+    php-pspell \
+    php-readline \
+    php-soap \
+    php-xml \
+    php-xmlrpc \
+    php-zip \
+    php-pear; \
+    openssl req -newkey rsa:2048 -x509 -sha256 -days 3650 -nodes -subj /CN=localhost -out /etc/ssl/server.pem -keyout /etc/ssl/server.key; \
+    openssl dhparam -out /etc/ssl/dhparams.pem 2048; \
+    service apache2 stop && a2enmod ssl rewrite; \
+    echo "; Custom PHP Settings" > /etc/php/01-custom.ini; \
+    ln -s /etc/php/01-custom.ini /etc/php/${PHP_VERSION}/apache2/conf.d/01-custom.ini; \
+    mkdir /var/log/php && chown -R www-data:www-data && chmod -R 0750; \
+    echo "Finished installing base system";
+
+RUN set eux; \
+    echo "Installing SQL Server PHP Extensions"; \
+    OS_BASE=$(sed -n 's/^VERSION_ID="\(.*\)"/\1/p' </etc/os-release); \
+    EXT_DIR=$(php-config --extension-dir); \
+    SQL_TMP="$(mktemp -d)"; \
+    OS_VERSION=$(echo "${OS_BASE}" | sed 's/[^0-9]*//g'); \
+    SQL_CAT=$(echo "${PHP_VERSION}" | sed 's/[^0-9]*//g'); \
+    wget "https://github.com/microsoft/msphpsql/releases/download/v${SQL_VERSION}/Ubuntu${OS_VERSION}-${PHP_VERSION}.tar" -O $SQL_TMP/mssql.tar; \
+    tar -xvf $SQL_TMP/mssql.tar -C $SQL_TMP --strip-components=1; \
+    mv $SQL_TMP/php_sqlsrv_${SQL_CAT}_nts.so $EXT_DIR/sqlsrv.so && mv $SQL_TMP/php_pdo_sqlsrv_${SQL_CAT}_nts.so $EXT_DIR/pdo_sqlsrv.so; \
+    rm -rf $SQL_TMP; \
+    printf "; priority=20\nextension=sqlsrv.so\n" > /etc/php/${PHP_VERSION}/mods-available/sqlsrv.ini; \
+    printf "; priority=30\nextension=pdo_sqlsrv.so\n" > /etc/php/${PHP_VERSION}/mods-available/pdo_sqlsrv.ini; \
+    phpenmod -v ${PHP_VERSION} sqlsrv pdo_sqlsrv; \
+    echo "Finished installing SQL Server PHP Extensions";
+
+COPY ./src/ ./
+
+VOLUME ["/var/www/html", "/var/log/apache2", "/var/log/php"]
+
+EXPOSE 80 443
+
+ENTRYPOINT ["/usr/local/bin/entrypoint.sh"]
---
+++ src/usr/local/bin/entrypoint.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+set -ex;
+    
+## Keygen ##
+echo "Keygen: Checking for certificate generation"
+if [[ -f "/etc/ssl/server.key" ]] || [[ -f "/etc/ssl/server.pem" ]];then
+    echo "Keygen: Certificate exist, checking if one should be generated"
+else
+    echo "Keygen: Certificate DOES NOT exist"
+    export GEN_KEYS=true
+fi
+    
+if [ ${GEN_KEYS} = true ];then
+    echo "Keygen: New Certificate will be generated"
+    openssl dhparam -out /etc/ssl/dhparams.pem 2048 && \
+    openssl req -newkey rsa:2048 -x509 -nodes \
+        -keyout /etc/ssl/server.key -new \
+        -out /etc/ssl/server.pem \
+        -subj /CN=localhost -sha256 -days 3650
+fi
+    
+PEM_SHA1=$(openssl x509 -noout -fingerprint -sha1 -in /etc/ssl/server.pem | cut -f2 -d"=" | sed "s/://g" | awk '{print tolower($0)}')
+echo "Keygen: Finished, Certificate Thumbprint: $PEM_SHA1"
+############
+    
+echo "CA Certificates: Checking for CA Import"
+if [[ ! -z "${CA_URL}" ]];then
+    echo "CA Certificates: The following URL will be searched ${CA_URL}"
+    LOCAL_STORE="/usr/local/share/ca-certificates"
+    cd /usr/local/share/ca-certificates
+    wget -r -nH -A *_CA.crt ${CA_URL}
+    for CA_CRT in /usr/local/share/ca-certificates/*.crt; do
+        CA_NAME=$(openssl x509 -noout -subject -nameopt multiline -in $CA_CRT | sed -n 's/ *commonName *= //p')
+        ${JAVA_HOME}/bin/keytool -import -trustcacerts -cacerts -storepass changeit -noprompt -alias "$CA_NAME" -file $CA_CRT >/dev/null 2>&1 | echo "CA Certificates: Added certificate to cacert, $CA_CRT"
+    done
+    update-ca-certificates
+else 
+    echo "CA Certificates: Nothing to import, CA_URL is not defined"
+fi
+    
+echo "Remote IP: Checking for Remote IP settings"
+if [[ ! -z "${VADC_IP_ADDRESS}" ]];then
+    echo "Remote IP: Found load a balancer ip address set, ${VADC_IP_ADDRESS} , attempting to configure modules"
+    if [[ -z "${VADC_IP_HEADER}" ]];then
+        echo "Remote IP: Found load a balancer ip address set but VADC_IP_HEADER was not found and is required, NOT configuring modules"
+    else 
+        a2enmod remoteip 
+        VADC_IP_REG=$(echo "${VADC_IP_ADDRESS}" | sed -e 's/\s/\|/g')
+        TOMCAT_SERVER_CFG=/opt/tomcat/conf/server.xml
+        MOD_REMOTE_IP=$(cat <<-EOF
+<IfModule remoteip_module>
+    RemoteIPInternalProxy ${VADC_IP_ADDRESS}
+    RemoteIPHeader ${VADC_IP_HEADER}
+</IfModule>
+EOF)
+        echo "$MOD_REMOTE_IP" > /etc/apache2/mods-enabled/remoteip.conf
+        echo "Remote IP: Apache2 module configured"
+    fi
+else 
+    echo "Remote IP: Did not find VADC_IP_ADDRESS and VADC_IP_HEADER, NOT configuring modules"
+fi
+    
+echo "Initialization complete, container ready"
+
+apachectl -k start -D FOREGROUND
